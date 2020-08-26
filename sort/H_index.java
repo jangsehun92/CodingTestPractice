32e84bd508f9a26c933d101b6dee035383afa8d0
jangsehun92 <jangsehun92@gmail.com>
2020-08-26 20:23:45 +0900

    h-index

--- sort/H_index.java
+++ sort/H_index.java
@@ -1,5 +1,8 @@
 package codingTest.sort;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class H_index {
 	/*
 	 * 
@@ -17,7 +20,57 @@ public class H_index {
 		이 과학자가 발표한 논문의 수는 5편이고, 그중 3편의 논문은 3회 이상 인용되었습니다. 그리고 나머지 2편의 논문은 3회 이하 인용되었기 때문에 이 과학자의 H-Index는 3입니다.
 	 */
 	public static void main(String[] args) {
+		//int[] citations = {3, 0, 6, 1, 5};
+		//int[] citations = {25, 8, 5, 3, 3};
+		int[] citations = {0, 0, 0};
 		
+		int answer = solution(citations);
+		
+		System.out.println(answer);
 	}
+	
+	public static int solution(int[] citations) {
+		int answer = 0;
+		
+		Integer[] arr = new Integer[citations.length];
+		
+		for(int i = 0; i < citations.length; i++) {
+			arr[i] = citations[i];
+		}
+		
+		Arrays.sort(arr, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+		});
+		
+		int h_index = arr[0];
+		
+		boolean isBoolean = true;
+		while(isBoolean) {
+			int count = 0;
+			if(arr[0] == 0) {
+				answer = count;
+				isBoolean = false;
+			}
+			
+			for(int i = 0; i < arr.length; i++) {
+				if(arr[i] >= h_index) {
+					count++;
+					System.out.println("h_index : " + h_index + " count : " + count);
+				}
+				
+				if(h_index == count) {
+					answer = count;
+					isBoolean = false;
+				}
+			}
+			h_index--;
+		}
+		
+		return answer;
+	}
+	
 
 }
