20f2c92f8b40993762941ca7598d5b04d1fc1445
jangsehun92 <jangsehun92@gmail.com>
2020-08-28 18:48:13 +0900

    주석 정리

--- sort/quickSort/QuickSort2.java
+++ sort/quickSort/QuickSort2.java
@@ -4,17 +4,17 @@ public class QuickSort2 {
 	
 	//재귀 함수는 메모리(스택)에 이전 함수(복사본)의 주소가 저장 : 마지막 호출된 복사본이 종료되면 이전 함수(복사본)으로 돌아감
 	public static void quickSort(int[] array, int left, int right) {
-		System.out.println("start - left : " + left + " right : " + right);
 		//정렬 후 오른쪽 섹션의 첫번째 인덱스를 받는다.
 		int rightFirstIndex = partition(array, left, right);
 		//왼쪽 섹션의 값이 1개 이상이여야 정렬한다.
-		if(left < rightFirstIndex-1) {
+		if(left < rightFirstIndex-1) { 
 			quickSort(array, left, rightFirstIndex-1);
 		}
 		//오른쪽 섹션 정렬
 		if(rightFirstIndex < right) {
 			quickSort(array, rightFirstIndex, right);
 		}
+		
 	}
 	
 	public static int partition(int[] array, int left, int right) {
