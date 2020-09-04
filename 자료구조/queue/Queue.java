24b8bc76bba318996d63c0fad5b758eb2975e990
jangsehun92 <jangsehun92@gmail.com>
2020-09-04 19:23:33 +0900

    queue 공부

--- 자료구조/queue/Queue.java
+++ 자료구조/queue/Queue.java
@@ -1,5 +1,13 @@
 package codingTest.자료구조.queue;
 
 public class Queue {
+	
+	/*	Queue에 사용되는 필수적인 메소드
+	 * enqueue 	: 큐의 가장 마지막에 데이터를 삽입한다.
+	 * dequeue 	: 큐의 첫 번째 위치에 있는 데이터를 반환하고 큐에서 삭제한다.
+	 * isEmpty 	: 큐가 empty 상태 인지 확인한다.
+	 * clear 	: 큐에 저장된 모든 데이터를 삭제하고 큐를 초기화한다.
+	 * peek 	: 큐의 첫 번째 위치에 있는 데이터를 추출한다.(데이터 삭제 하지 않음)
+	 */
 
 }
