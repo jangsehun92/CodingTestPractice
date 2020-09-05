e4094dc05e31d051f527e1feb214baab040003b7
jangsehun92 <jangsehun92@gmail.com>
2020-09-05 23:44:15 +0900

    일일 sort

--- 자료구조/queue/Queue.java
+++ 자료구조/queue/Queue.java
@@ -3,6 +3,7 @@ package codingTest.자료구조.queue;
 public class Queue {
 	
 	/*	Queue에 사용되는 필수적인 메소드
+	 * 선입선출(FIFO)
 	 * enqueue 	: 큐의 가장 마지막에 데이터를 삽입한다.
 	 * dequeue 	: 큐의 첫 번째 위치에 있는 데이터를 반환하고 큐에서 삭제한다.
 	 * isEmpty 	: 큐가 empty 상태 인지 확인한다.
