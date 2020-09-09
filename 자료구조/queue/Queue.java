60813cdc597afaa628b46cad3a3bd21fa896f513
jangsehun92 <jangsehun92@gmail.com>
2020-09-09 19:05:44 +0900

    일일 sort

--- 자료구조/queue/Queue.java
+++ 자료구조/queue/Queue.java
@@ -10,5 +10,65 @@ public class Queue {
 	 * clear 	: 큐에 저장된 모든 데이터를 삭제하고 큐를 초기화한다.
 	 * peek 	: 큐의 첫 번째 위치에 있는 데이터를 추출한다.(데이터 삭제 하지 않음)
 	 */
+	
+	private int[] queue;
+	private int queueSize;
+	private int front;	//데이터 삭제 index
+	private int rear;	//마지막 데이터 삽입 index값
+	
+	public Queue(int queueSize) {
+		queue = new int[queueSize];
+		this.queueSize = queueSize;
+		this.front = -1;
+		this.rear = -1;
+	}
+	
+	public void enqueue(int data) {
+		System.out.println("Enqueue [ " + data + " ]");
+		queue[++rear] = data;
+	}
+	
+	public int dequeue() {
+		if(isEmpty()) {
+			throw new ArrayIndexOutOfBoundsException("큐가 비어있습니다.");
+		}
+		//front = (front + 1) % queueSize;
+		System.out.println("Dequeue [ " + queue[++front] + " ]");
+		return queue[front];
+	}
+	
+	public int peek() {
+		if(isEmpty()) {
+			throw new ArrayIndexOutOfBoundsException("큐가 비어있습니다.");
+		}
+		System.out.println("Peek [ " + queue[front+1] +" ]");
+		return queue[front+1];
+	}
+	
+	public boolean isEmpty() {
+		if(front == rear) {
+			front = -1;
+			rear = -1;
+		}
+		return (front == rear);
+	}
+	
+	public void clear() {
+		queue = new int[queueSize];
+		front = -1;
+		rear = -1;
+	}
+	
+	public void printQueue() {
+		if(isEmpty()) {
+			throw new ArrayIndexOutOfBoundsException("큐가 비어있습니다.");
+		}
+		System.out.println("Queue List");
+		for(int i = front+1; i <= rear; i++) {
+			System.out.print(queue[i]);
+		}
+		System.out.println();
+	}
+	
 
 }
--- 자료구조/queue/QueueMain.java
+++ 자료구조/queue/QueueMain.java
@@ -3,8 +3,22 @@ package codingTest.자료구조.queue;
 public class QueueMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Queue");
+		Queue queue = new Queue(5);
+		
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+		queue.enqueue(4);
+		queue.enqueue(5);
+		queue.printQueue();
+		queue.dequeue();
+		queue.dequeue();
+		queue.dequeue();
+		queue.dequeue();
+		queue.peek();
+		queue.printQueue();
 	}
 
 }
