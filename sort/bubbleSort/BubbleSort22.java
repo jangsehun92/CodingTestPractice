e83fbd052495625ecddf4cfb953412c58dca21e8
jangsehun92 <jangsehun92@gmail.com>
2020-09-19 20:42:34 +0900

    일일 sort

---
+++ sort/bubbleSort/BubbleSort22.java
@@ -0,0 +1,39 @@
+package codingTest.sort.bubbleSort;
+
+public class BubbleSort22 {
+	
+	public static void bubbleSort(int[] arr, int last) {
+		if(last > 0) {
+			for(int i = 1; i <= last; i++) {
+				if(arr[i-1] > arr[i]){
+					swap(arr, i-1, i);
+				}
+			}
+			bubbleSort(arr, last-1);
+		}
+	}
+	
+	public static void swap(int[] arr, int left, int right) {
+		int temp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = temp;
+	}
+	
+	public static void print(int[] arr) {
+		for(int data : arr) {
+			System.out.print(data + ", ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1,7,0,4,9,5,6,3,8,2 };
+		
+		print(arr);
+		bubbleSort(arr, arr.length-1);
+		print(arr);
+		
+
+	}
+
+}
