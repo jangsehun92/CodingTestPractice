d45951a15d05374ee667af50f2ae13870f6f2d17
jangsehun92 <jangsehun92@gmail.com>
2020-10-03 19:38:00 +0900

    일일 sort

---
+++ sort/selectionSort/SelectionSort4.java
@@ -0,0 +1,37 @@
+package codingTest.sort.selectionSort;
+
+public class SelectionSort4 {
+	
+	public static void selectionSort(int[] arr, int start) {
+		if(start < arr.length-1) {
+			int minIndex = start;
+			for(int i = start; i < arr.length; i++) {
+				if(arr[minIndex] > arr[i]) minIndex = i;
+			}
+			swap(arr, start, minIndex);
+			selectionSort(arr, start+1);
+		}
+	}
+	
+	public static void swap(int[] arr, int left, int right) {
+		int temp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = temp;
+	}
+	
+	public static void print(int[] arr) {
+		for(int data : arr) {
+			System.out.print(data + ", ");
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] arr = {1,6,8,9,0,7,4,2,3,5};
+		print(arr);
+		selectionSort(arr, 0);
+		print(arr);
+	}
+
+}
