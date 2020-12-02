8ef10218cf3fe8b16d769f49063f90ae6ec9ad9f
jangsehun92 <jangsehun92@gmail.com>
2020-12-02 15:48:22 +0900

    일일 sort

---
+++ sort/selectionSort/SelectionSort24.java
@@ -0,0 +1,31 @@
+package codingTest.sort.selectionSort;
+
+import codingTest.sort.util.ArrayMaker;
+import codingTest.sort.util.Util;
+
+public class SelectionSort24 {
+	
+	public static void selectionSort(int[] arr, int start, Util util) {
+		if(start < arr.length-1) {
+			int minIndex = start;
+			for(int i = start+1; i < arr.length; i++) {
+				if(arr[minIndex] > arr[i]) minIndex = i;
+			}
+			util.swap(arr, start, minIndex);
+			selectionSort(arr, start+1, util);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Util util = new Util();
+		ArrayMaker arrMaker = new ArrayMaker(15);
+		int[] arr = arrMaker.getArray();
+		
+		util.print(arr);
+		selectionSort(arr, 0, util);
+		util.print(arr);
+				
+		
+	}
+
+}
